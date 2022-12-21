4c4ff8bd2ae73c98c410a31db4ed27b2e14f2095
Oscar Levin <oscar.levin@unco.edu>
2022-12-21 15:35:27 +0000

    move output to docs

---
+++ docs/web/lunr-pretext-search-index.js
@@ -0,0 +1,310 @@
+var ptx_lunr_search_style = "textbook";
+var ptx_lunr_docs = [
+{
+  "id": "colophon-1",
+  "level": "1",
+  "url": "colophon-1.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": "  my-website.org  http:\/\/pretextbook.org   copyright  "
+},
+{
+  "id": "dedication-1",
+  "level": "1",
+  "url": "dedication-1.html",
+  "type": "Dedication",
+  "number": "",
+  "title": "Dedication",
+  "body": " For ...  "
+},
+{
+  "id": "acknowledgement-1",
+  "level": "1",
+  "url": "acknowledgement-1.html",
+  "type": "Acknowledgements",
+  "number": "",
+  "title": "Acknowledgements",
+  "body": " I would like to thank...  "
+},
+{
+  "id": "meta_frontmatter-preface",
+  "level": "1",
+  "url": "meta_frontmatter-preface.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Preface",
+  "body": "   About this book:  "
+},
+{
+  "id": "preface-2",
+  "level": "1",
+  "url": "preface-2.html",
+  "type": "Preface",
+  "number": "",
+  "title": "Not just that..",
+  "body": " Not just that..  I still have more to say!  "
+},
+{
+  "id": "sec_first-intro",
+  "level": "1",
+  "url": "sec_first-intro.html",
+  "type": "Section",
+  "number": "1.1",
+  "title": "Introduction to First Chapter",
+  "body": " Introduction to First Chapter   Intro to section, because we will have subsections.  Another paragraph of the introduction.    A subsection  Content of the subsection.  More content.    Another Subsection  Text with some math: only for some values of .    After the last subsection, you might have a conclusion.   "
+},
+{
+  "id": "sec_first-examples",
+  "level": "1",
+  "url": "sec_first-examples.html",
+  "type": "Section",
+  "number": "1.2",
+  "title": "Examples",
+  "body": " Examples  Here is an example:    The statement of the example, since it will have a solution. What is the solution?    This is the solution.    Here is another example, but without a solution   This is an example of an example without a solution (or hint or answer) so the paragraph doesn't need to be inside a statement tag.   Although the next things are not examples, they give examples of blocks that are not examples. First a theorem.   Theorem Title   This is the statement of the theorem.    And this is its proof.      A corollary that doesn't need a proof.    Perhaps at the end of the section, you want to add a note to the reader about how what you have fits into the larger scope of mathematics.   Larger Context  Comment to the reader here.   Of course, if you want more information, you might see .  "
+},
+{
+  "id": "example-1",
+  "level": "2",
+  "url": "sec_first-examples.html#example-1",
+  "type": "Example",
+  "number": "1.2.1",
+  "title": "",
+  "body": "  The statement of the example, since it will have a solution. What is the solution?    This is the solution.   "
+},
+{
+  "id": "example-2",
+  "level": "2",
+  "url": "sec_first-examples.html#example-2",
+  "type": "Example",
+  "number": "1.2.2",
+  "title": "",
+  "body": " This is an example of an example without a solution (or hint or answer) so the paragraph doesn't need to be inside a statement tag.  "
+},
+{
+  "id": "thm_first-examples-theorem",
+  "level": "2",
+  "url": "sec_first-examples.html#thm_first-examples-theorem",
+  "type": "Theorem",
+  "number": "1.2.3",
+  "title": "Theorem Title.",
+  "body": " Theorem Title   This is the statement of the theorem.    And this is its proof.   "
+},
+{
+  "id": "cor_first-examples-corollary",
+  "level": "2",
+  "url": "sec_first-examples.html#cor_first-examples-corollary",
+  "type": "Corollary",
+  "number": "1.2.4",
+  "title": "",
+  "body": "  A corollary that doesn't need a proof.   "
+},
+{
+  "id": "remark_first-examples-context",
+  "level": "2",
+  "url": "sec_first-examples.html#remark_first-examples-context",
+  "type": "Remark",
+  "number": "1.2.5",
+  "title": "Larger Context.",
+  "body": " Larger Context  Comment to the reader here.  "
+},
+{
+  "id": "ex_first",
+  "level": "1",
+  "url": "ex_first.html",
+  "type": "Exercises",
+  "number": "1.3",
+  "title": "My Exercises",
+  "body": "  My Exercises   Is this an exercise?   Yes.   It is, but not a very good one.    Another, with a hint.   The hint.      "
+},
+{
+  "id": "exercise-1",
+  "level": "2",
+  "url": "ex_first.html#exercise-1",
+  "type": "Exercise",
+  "number": "1.3.1",
+  "title": "",
+  "body": " Is this an exercise?   Yes.   It is, but not a very good one.  "
+},
+{
+  "id": "exercise-2",
+  "level": "2",
+  "url": "ex_first.html#exercise-2",
+  "type": "Exercise",
+  "number": "1.3.2",
+  "title": "",
+  "body": " Another, with a hint.   The hint.  "
+},
+{
+  "id": "ch_empty",
+  "level": "1",
+  "url": "ch_empty.html",
+  "type": "Chapter",
+  "number": "2",
+  "title": "Interactive Activities that Don't Work?",
+  "body": " Interactive Activities that Don't Work?  These checkpoints don't work. But the same code produces working ones in an exercises division below.  True\/False Question  A True\/false question   Feedback   Matching Problem  A multiple choice question     right answer 1    answer specific feedback      right answer 1    answer specific feedback      wrong answer 1    answer specific feedback      wrong answer 2    answer specific feedback     Parsons Problem  Rearrange the blocks in alphebetical order       A    B    C    D         Matching Problem  Match the letters with their order in the alphabet    A  1    B  2    C  3     True\/False Question  A True\/false question   Feedback   Matching Problem  A multiple choice question     right answer 1    answer specific feedback      right answer 1    answer specific feedback      wrong answer 1    answer specific feedback      wrong answer 2    answer specific feedback     Parsons Problem  Rearrange the blocks in alphebetical order       A    B    C    D         Matching Problem  Match the letters with their order in the alphabet    A  1    B  2    C  3     "
+},
+{
+  "id": "exercise-3",
+  "level": "2",
+  "url": "ch_empty.html#exercise-3",
+  "type": "Checkpoint",
+  "number": "2.0.1",
+  "title": "True\/False Question.",
+  "body": "True\/False Question  A True\/false question   Feedback  "
+},
+{
+  "id": "exercise-4",
+  "level": "2",
+  "url": "ch_empty.html#exercise-4",
+  "type": "Checkpoint",
+  "number": "2.0.2",
+  "title": "Matching Problem.",
+  "body": "Matching Problem  A multiple choice question     right answer 1    answer specific feedback      right answer 1    answer specific feedback      wrong answer 1    answer specific feedback      wrong answer 2    answer specific feedback    "
+},
+{
+  "id": "exercise-5",
+  "level": "2",
+  "url": "ch_empty.html#exercise-5",
+  "type": "Checkpoint",
+  "number": "2.0.3",
+  "title": "Parsons Problem.",
+  "body": "Parsons Problem  Rearrange the blocks in alphebetical order       A    B    C    D        "
+},
+{
+  "id": "exercise-6",
+  "level": "2",
+  "url": "ch_empty.html#exercise-6",
+  "type": "Checkpoint",
+  "number": "2.0.4",
+  "title": "Matching Problem.",
+  "body": "Matching Problem  Match the letters with their order in the alphabet    A  1    B  2    C  3   "
+},
+{
+  "id": "exercise-7",
+  "level": "2",
+  "url": "exercises-Working.html#exercise-7",
+  "type": "Exercise",
+  "number": "2.1",
+  "title": "True\/False Question.",
+  "body": "True\/False Question  A True\/false question   Feedback  "
+},
+{
+  "id": "exercise-8",
+  "level": "2",
+  "url": "exercises-Working.html#exercise-8",
+  "type": "Exercise",
+  "number": "2.2",
+  "title": "Matching Problem.",
+  "body": "Matching Problem  A multiple choice question     right answer 1    answer specific feedback      right answer 1    answer specific feedback      wrong answer 1    answer specific feedback      wrong answer 2    answer specific feedback    "
+},
+{
+  "id": "exercise-9",
+  "level": "2",
+  "url": "exercises-Working.html#exercise-9",
+  "type": "Exercise",
+  "number": "2.3",
+  "title": "Parsons Problem.",
+  "body": "Parsons Problem  Rearrange the blocks in alphebetical order       A    B    C    D        "
+},
+{
+  "id": "exercise-10",
+  "level": "2",
+  "url": "exercises-Working.html#exercise-10",
+  "type": "Exercise",
+  "number": "2.4",
+  "title": "Matching Problem.",
+  "body": "Matching Problem  Match the letters with their order in the alphabet    A  1    B  2    C  3   "
+},
+{
+  "id": "sec_features-blocks",
+  "level": "1",
+  "url": "sec_features-blocks.html",
+  "type": "Section",
+  "number": "3.1",
+  "title": "Environments and Blocks",
+  "body": " Environments and Blocks  Some text   My Theorem   Theorem statement.    Proof of theorem.      Statement of example    The solution.    Now a figure.   A frog   A nice-looking frog with a longish description.    "
+},
+{
+  "id": "thm_features-blocks-my-theorem",
+  "level": "2",
+  "url": "sec_features-blocks.html#thm_features-blocks-my-theorem",
+  "type": "Theorem",
+  "number": "3.1.1",
+  "title": "My Theorem.",
+  "body": " My Theorem   Theorem statement.    Proof of theorem.   "
+},
+{
+  "id": "example-3",
+  "level": "2",
+  "url": "sec_features-blocks.html#example-3",
+  "type": "Example",
+  "number": "3.1.2",
+  "title": "",
+  "body": "  Statement of example    The solution.   "
+},
+{
+  "id": "figure-1",
+  "level": "2",
+  "url": "sec_features-blocks.html#figure-1",
+  "type": "Figure",
+  "number": "3.1.3",
+  "title": "",
+  "body": " A frog   A nice-looking frog with a longish description.   "
+},
+{
+  "id": "solutions-1",
+  "level": "1",
+  "url": "solutions-1.html",
+  "type": "Appendix",
+  "number": "A",
+  "title": "Selected Hints",
+  "body": " Selected Hints  "
+},
+{
+  "id": "solutions-2",
+  "level": "1",
+  "url": "solutions-2.html",
+  "type": "Appendix",
+  "number": "B",
+  "title": "Selected Solutions",
+  "body": " Selected Solutions  "
+},
+{
+  "id": "appendix-1",
+  "level": "1",
+  "url": "appendix-1.html",
+  "type": "Appendix",
+  "number": "C",
+  "title": "List of Symbols",
+  "body": " List of Symbols   "
+},
+{
+  "id": "index-1",
+  "level": "1",
+  "url": "index-1.html",
+  "type": "Index",
+  "number": "",
+  "title": "Index",
+  "body": " Index   "
+},
+{
+  "id": "colophon-2",
+  "level": "1",
+  "url": "colophon-2.html",
+  "type": "Colophon",
+  "number": "",
+  "title": "Colophon",
+  "body": " This book was authored in PreTeXt .  "
+}
+]
+
+var ptx_lunr_idx = lunr(function () {
+  this.ref('id')
+  this.field('title')
+  this.field('body')
+
+  ptx_lunr_docs.forEach(function (doc) {
+    this.add(doc)
+  }, this)
+})
